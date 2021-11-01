bffdafec7f6485cdced20c6b7ca4c3fef51d9b68
Ching <ndchinh2003@gmail.com>
2021-11-01 09:58:28 +0700

    changes

--- scratch_3.py
+++ scratch_3.py
@@ -3,7 +3,7 @@ def sqrt2(n):
     if n==1: return 1.4
     else:
      return math.sqrt(2+sqrt2(n-1))
-print(sqrt2(10))
 #imadesomechanges here
 #there is also some changes here
 #commits on new branch
+#similar con c
